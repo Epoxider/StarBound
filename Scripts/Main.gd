1260f73e9f611b61ea2b57dd264b71cf70718076
Epoxide <matthew.r.fritz@gmail.com>
2025-09-07 13:24:42 -0700

    small main cleanup

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,9 +1,6 @@
 extends Node2D
 
-
-@onready var bullet_manager = $BulletManager
 @onready var player = $Player
-@onready var beholder = $Beholder
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
